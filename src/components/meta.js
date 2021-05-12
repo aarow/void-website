7af3165ba23d9082027f33a08444e6441caeb8e6
Aaron <mrmustarde@me.com>
2021-05-12 09:21:40 -0700

    sass

--- src/components/meta.js
+++ src/components/meta.js
@@ -35,6 +35,16 @@ export default function Meta() {
       {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       {/* <meta name="description" content={SITE_NAME} /> */}
       {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      {/* <link
+        href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600&display=swap"
+        rel="stylesheet"
+      /> */}
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600&family=Mulish:ital,wght@0,200;0,300;0,400;1,400&display=swap"
+        rel="stylesheet"
+      />
     </>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../css/index.css";
+import "../scss/index.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
