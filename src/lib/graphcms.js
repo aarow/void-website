134934e2aea8386ed0761c513e4dceb0db34e14f
Aaron <mrmustarde@me.com>
2021-05-18 08:58:20 -0700

    team page

--- src/lib/graphcms.js
+++ src/lib/graphcms.js
@@ -4,71 +4,12 @@ const graphcms = new GraphQLClient(
   "https://api-us-west-2.graphcms.com/v2/cknmqf56zvo9z01uteckkbkch/master"
 );
 
-export async function getPage(slug) {
-  const { page } = await graphcms.request(
-    `{
-      page(where: {slug: "home"}) {
-        title
-        pageSections {
-          id
-          items {
-            __typename
-            ... on TeamMember {
-              id
-              name
-              biography {
-                html
-              }
-              biographyExcerpt
-              portrait {
-                url(transformation: {image: {resize: {width: 512}}})
-              }
-              position
-            }
-            ... on Hero {
-              id
-              content {
-                html
-              }
-              height
-              title
-              backgroundImage {
-                url
-              }
-              backgroundVideo {
-                url
-              }
-              cssClass
-            }
-            ... on Cta {
-              id
-              header
-              title
-              header
-              subHeader
-              content
-              centered
-              icon {
-                url
-              }
-              linkUrl
-              linkTitle
-              backgroundColor {
-                hex
-              }
-              textColor {
-                hex
-              }
-              whiteText
-            }
-          }
-        }
-      }
-    }
-    
-    
-      `
-  );
+export async function getPage(slugName) {
+  const variables = {
+    slug: slugName,
+  };
+
+  const { page } = await graphcms.request(query, variables);
 
   return {
     props: {
@@ -76,3 +17,69 @@ export async function getPage(slug) {
     },
   };
 }
+
+const query = `query getPage($slug: String!) {
+  page(where: {slug: $slug}) {
+    title
+    subtitle
+    id
+    content {
+      html
+    }
+    pageSections {
+      id
+      items {
+        __typename
+        ... on TeamMember {
+          id
+          name
+          biography {
+            html
+          }
+          biographyExcerpt
+          portrait {
+            url(transformation: {image: {resize: {width: 512}}})
+          }
+          position
+        }
+        ... on Hero {
+          id
+          content {
+            html
+          }
+          height
+          title
+          backgroundImage {
+            url
+          }
+          backgroundVideo {
+            url
+          }
+          cssClass
+        }
+        ... on Cta {
+          id
+          header
+          title
+          header
+          subHeader
+          content
+          centered
+          icon {
+            url
+          }
+          linkUrl
+          linkTitle
+          backgroundColor {
+            hex
+          }
+          textColor {
+            hex
+          }
+          whiteText
+        }
+      }
+    }
+  }
+}
+`;
---
+++ src/pages/team/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { getPage } from "../../lib/graphcms";
+import { SITE_NAME } from "../../lib/constants";
+import Layout from "../../components/layout";
+
+export async function getStaticProps(foo) {
+  console.log("foo: ", foo);
+  return await getPage("team");
+}
+
+export default function TeamPage(props) {
+  const {
+    page: { title, subtitle, id, content, pageSections },
+  } = props;
+
+  return (
+    <Layout>
+      <Head>
+        <title>
+          {title} | {SITE_NAME}
+        </title>
+      </Head>
+
+      <section>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+        <div dangerouslySetInnerHTML={{ __html: content.html }} />
+      </section>
+
+      {pageSections.map((pageSection) => (
+        <section key={pageSection.id}>{pageSection.id}</section>
+      ))}
+    </Layout>
+  );
+}
