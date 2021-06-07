8187118e6baed5f5db3a5c6cbc078794947e683b
Aaron <mrmustarde@me.com>
2021-06-06 21:44:47 -0700

    netlify no index contact page yet

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,18 @@
+import Head from "next/head";
+
 export default function ContactPage(props) {
   return (
-    <form name="contact" data-netlify="true">
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <input type="text" name="phone" />
-      <select name="subject" />
-      <textarea name="message"></textarea>
-    </form>
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <form name="contact" data-netlify="true">
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="text" name="phone" />
+        <select name="subject" />
+        <textarea name="message"></textarea>
+      </form>
+    </>
   );
 }
