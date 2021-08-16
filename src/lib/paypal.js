787d6cdabbdf882106cb1555a41bfcf282d4888a
Aaron <mrmustarde@me.com>
2021-08-16 09:44:26 -0700

    paypal client id

--- src/lib/paypal.js
+++ src/lib/paypal.js
@@ -2,5 +2,5 @@ export const SANDBOX_ACCT = `sb-phkf67162079@business.example.com`;
 export const SANDBOX_ACCT_CLIENT_ID = `AXC4sTiDEcH8LbG10fTGsJmY0bY1mW2K0EH9c6JhhwF_Y2CueAjGo-2jIMTOzaJ6dxdsbgPoB-g483Ax`;
 
 export const ACCT_EMAIL = `sb-phkf67162079@business.example.com`;
-export const CLIENT_ID = `AXC4sTiDEcH8LbG10fTGsJmY0bY1mW2K0EH9c6JhhwF_Y2CueAjGo-2jIMTOzaJ6dxdsbgPoB-g483Ax`;
+export const CLIENT_ID = `ATAPvb8ylhy2bWOHRB84ylTLVjUums3lQQ-fXqjgVlAbkhPv2b3ej2CjMv1RJljZo0nqo8lsov2_PG4B`;
 export const CURRENCY = `USD`;
